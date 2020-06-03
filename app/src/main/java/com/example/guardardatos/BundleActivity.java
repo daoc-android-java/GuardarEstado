565bc88aadb8931f96189dca313498eda454c00e
Diego Ordóñez <diego.ordonez@gmail.com>
2020-06-03 10:26:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/guardardatos/BundleActivity.java
@@ -0,0 +1,54 @@
+package com.example.guardardatos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class BundleActivity extends Activity {
+
+	private String msg;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		/*
+  		//Si restaura el estado aqui, debe verificar si el bundle no es null
+		if(savedInstanceState != null) {
+			//si comenta esta linea, no se recupera el estado guardado
+			msg = savedInstanceState.getString("MSG");
+		}
+		*/
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);//Siempre llame a super (guarda estado de Views)
+		//si comenta esta linea no se guarda el estado
+		//pruebe cambiando la orientacion de la pantalla
+		//outState.putString("MSG", msg);
+	}
+
+	//Solo se invoca si el bundle no es null
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+	    super.onRestoreInstanceState(savedInstanceState);//Siempre llame a super (restaura estado de Views)
+		//si comenta esta linea no se restaura el estado
+		//pruebe cambiando la orientacion de la pantalla
+	    //msg = savedInstanceState.getString("MSG");
+	}
+
+	public void guardaMensaje(View v) {
+		EditText et = (EditText) findViewById(R.id.msg);
+		msg = et.getText().toString();
+	}
+
+	public void recuperaMensaje(View v) {
+		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+	}
+
+}
